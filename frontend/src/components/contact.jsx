ad5b50ddda38ca33060e674533ad395874e5ddc7
therohitshelar97 <rohidasshelar1997@gmail.com>
2024-04-06 18:36:03 +0530

    'frontend-with-django'

---
+++ frontend/src/components/contact.jsx
@@ -0,0 +1,11 @@
+export default function Contact(){
+    return(
+        <>
+        <div className="container">
+            <form>
+                Name:<input type="text" name="" class="form-control" />
+            </form>
+        </div>
+        </>
+    )
+}
